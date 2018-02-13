83b669fb2d00f06caa56ec02d8e4d0dc009c0b89
Caleb McHenry <calebmchenry@The-Real-Caleb.local>
2018-02-13 15:25:00 -0500

    changes

--- spit.py
+++ spit.py
@@ -9,14 +9,19 @@ import math
 import scipy as sp
 from scipy import signal
 
-DM1 = 80
-Sig1 = PSS.Signal(TotTime = 50, bw = 40, SignalType = 'voltage', data_type = 'int16')
+dispersionMeasure = 80
+signal = PSS.Signal(TotTime = 50, bw = 40, SignalType = 'voltage', data_type = 'int16')
 
-Psr1 = PSS.Pulsar(Sig1)
+Psr1 = PSS.Pulsar(signal)
 Psr1.make_pulses()
 
-print(Psr1.signal[0,:])
-print(bytes(Psr1.signal[0,:]))
-psrSigFile = open('../PulsarSignalOutputs/pulsarSignalFile.bin', 'wb')
+# print(Psr1.signal[0,:])
+# print(bytes(Psr1.signal[0,:]))
+
+psrSigFile = open('PulsarSignalOutputs/pulsarSignalFile.bin', 'wb')
 psrSigFile.write(bytes(Psr1.signal[0,:]))
+psrSigFile.close()
+
+
+
 
