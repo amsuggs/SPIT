3bd5a115c7120f52d1951bed298e0ffc6deda30f
Caleb McHenry <>
2018-03-25 21:49:12 -0400

    caleb-chirp: created test chirp file and edited spit file to produced binary file based off of what paul told us

--- spyt.py
+++ spyt.py
@@ -18,10 +18,13 @@ dm = 1.0/(math.pow(f, 2.0))
 
 file = open('PulsarSignalOutputs/spyt.bin', 'wb')
 for bin in range(0, p):
-    x = 2 * math.pi * f * bin * p
+    x = 2 * math.pi * bin / p
     real = math.sin(x)
     imag = math.cos(x)
-    print(real)
-    print(imag)
+    file.write(np.int8(real * 100))
+    file.write(np.int8(imag * 100))
+    print(x)
+    print(real * 100)
+    print(imag * 100)
     print("---")
 file.close()
